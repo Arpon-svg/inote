b51ca53ec81498323ae5751af059065070ec2d55
Arpon <a4arpon@gmail.com>
2022-11-27 01:29:17 +0600

    Trying to setup mongodb

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
-const mongoURl = "mongodb://localhost:27017/";
+const mongoURl = "mongodb://localhost:27017/inote.users";
 // Now we need to connect to the database
+// My DB name is inote
+// I want to save my users auth details to users collection
 const db = () => {
-    mongoose.connect(mongoURl, () => { console.log('Connected to MongoSuccessfully');; }); 
+    mongoose.connect(mongoURl, { dbName: inote }, () => { console.log('Connected to MongoSuccessfully'); });
 }
-module.exports  = db;
+module.exports = db;
